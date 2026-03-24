472d8cc8440b2579ed42965e99754f94d3816a10
LiamAnderson1 <102837362+LiamAnderson1@users.noreply.github.com>
2026-03-24 16:11:03 -0400

    Improved battle UI and added API intergration

--- frontend/cerebrawl-frontend/src/App.jsx
+++ frontend/cerebrawl-frontend/src/App.jsx
@@ -1,8 +1,33 @@
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import BattleScreen from "./components/BattleScreen";
-import "./App.css";
+
+function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="home-container">
+      <h1 className="title">CereBrawl</h1>
+      <p className="subtitle">Get ready to enter the battle.</p>
+
+      <button
+        className="start-button"
+        onClick={() => navigate("/battlescreen")}
+      >
+        Start Battle
+      </button>
+    </div>
+  );
+}
 
 function App() {
-  return <BattleScreen />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/battlescreen" element={<BattleScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
